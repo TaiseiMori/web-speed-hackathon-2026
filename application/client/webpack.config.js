f8d9b591ef37348e8f024c4c9e9f0e1a8516c18f
TaiseiMori <taisei.mrkw@gmail.com>
2026-03-20 16:32:09 +0900

    fix: webpackのモードをproductionに変更

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -65,7 +65,7 @@ const config = {
       [...commonEntryPrefix, path.resolve(SRC_PATH, file)],
     ]),
   ),
-  mode: "none",
+  mode: "production",
   module: {
     rules: [
       {
@@ -103,7 +103,7 @@ const config = {
       BUILD_DATE: new Date().toISOString(),
       // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
       COMMIT_HASH: process.env.SOURCE_VERSION || "",
-      NODE_ENV: "development",
+      NODE_ENV: "production",
     }),
     new MiniCssExtractPlugin({
       filename: "styles/[name].css",
