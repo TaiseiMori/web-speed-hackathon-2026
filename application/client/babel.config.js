bef50476f202dca703ba2d3d86e7cfae1a55be71
TaiseiMori <taisei.mrkw@gmail.com>
2026-03-21 13:32:16 +0900

    fix: BabelとWebpackの設定を最新のブラウザターゲットに変更

--- application/client/babel.config.js
+++ application/client/babel.config.js
@@ -4,9 +4,9 @@ module.exports = {
     [
       "@babel/preset-env",
       {
-        targets: "ie 11",
+        targets: "last 2 Chrome versions, last 2 Firefox versions, last 2 Safari versions",
         corejs: "3",
-        modules: "commonjs",
+        modules: false,
         useBuiltIns: false,
       },
     ],
